716502266b5a1b5293bed2f068aacff268ab4555
Grey Larrain Bratt <greybratt@outlook.com>
2022-05-14 16:54:32 -0700

    functioning lab 6

--- js/main.js
+++ js/main.js
@@ -1,10 +1,77 @@
-//This code does NOT create any global variables.
-//Promises can be chained together, with the previous promise
-// passing its results to the next one in the chain.
-// the format is: fetch().then().then().catch()
-//it's easier to read if we put each step in its own line,
-//that's why the periods start the then lines.
+const container = document.querySelector("#container");
+const body = document.getElementById("main");
+let boxes = [container, body]
 
+// initializing page
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    getHouses();
+    getColors();
+}
+
+async function getHouses() {
+    try {
+        let response = await fetch("houses.json");
+        let data = await response.json();
+
+        //create a temp holder to append all the html generated inside the forEach iterator
+        let html = "";
+
+        //the argument "house" passed to the arrow function
+        //holds each item in the array in turn.fetch("houses.json")
+        data.forEach(house => {
+            let family = house.members.join(", ");
+
+            // generate the html snippet for one array item
+            //to be added to the "html" temp holder.
+            let objInfo =
+            `<dl>
+                <dt class="house">${house.name}</dt>
+                <dd class="people">${family}</dd>
+            </dl>`;
+            html += objInfo;
+        });
+        
+        container.innerHTML = html;
+    } catch(err) {
+        console.log("Houses Err", err)
+    };
+};
+
+
+
+async function getColors() {
+    try {
+        let response = await fetch("https://x-colors.herokuapp.com/api/random?number=2");
+        let data = await response.json();
+        // put one color on each box background
+        for (let i=0; i < boxes.length; i++) {
+            let color = data[i].rgb;
+            let box = boxes[i];
+            box.style.backgroundColor = color;
+        };
+    } catch(err) {
+        console.log("Color Error", err);
+    };
+};
+
+
+/*
+fetch("colors.json")
+    .then((response) => response.json())
+    .then(data => {
+        data.forEach(color => {
+            let codes = color.join(", ");
+        });
+    })
+    .catch(err => console.log("Color error!", err)); */
+    /*color => {
+        let bgColor = color[0].rgb.value;
+        const bg = document.querySelector("#main");
+        console.log(bg, bgColor);*/
+
+/*
 fetch("houses.json")
     .then((response) => response.json())
     .then((data) => {
@@ -33,25 +100,31 @@ fetch("houses.json")
     })
     .catch(err => console.log("GoT error!", err));
     //this only runs if there is an error during the above process
-
-fetch("colors.json")
+ fetch("houses.json")
     .then((response) => response.json())
-    .then(data => {
+    .then((data) => {
+        //create a temp holder to append all the html generated inside the forEach iterator
         let html = "";
-        data.forEach(color => {
-            let codes = color.join(", ");
+
+        //the argument "house" passed to the arrow function
+        //holds each item in the array in turn.fetch("houses.json")
+        data.forEach(house => {
+            let family = house.members.join(", ");
+
+            // generate the html snippet for one array item
+            //to be added to the "html" temp holder.
             let objInfo =
             `<dl>
-                <dt class="house">${codes}</dt>
+                <dt class="house">${house.name}</dt>
                 <dd class="people">${family}</dd>
             </dl>`;
             html += objInfo;
         });
+
+        //make a reference to the html container where
+        //the info will be displayed.
         const container = document.querySelector("#container");
         container.innerHTML = html;
-        /*color => {
-        let bgColor = color[0].rgb.value;
-        const bg = document.querySelector("#main");
-        console.log(bg, bgColor);*/
     })
-    .catch(err => console.log("Color error!", err));
+    .catch(err => console.log("GoT error!", err));
+    //this only runs if there is an error during the above process */
