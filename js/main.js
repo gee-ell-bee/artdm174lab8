236d3919acc3e648042ab851e38307f1aedf7cca
Grey Larrain Bratt <greybratt@outlook.com>
2022-05-18 16:09:09 -0700

    + take color db info to use in DOM

--- js/main.js
+++ js/main.js
@@ -7,78 +7,114 @@ let boxes = [container, body];
 // color buttons
 const newColorsBtn = document.getElementById("new");
 const storeColorsBtn = document.getElementById("store");
+const clearColsBtn = document.getElementById("clearStor");
 
-// initializing page
-document.addEventListener("DOMContentLoaded", init);
+function clearStorage(db) {
+    db.colors.clear();
+}
 
-function init() {
-    //inputHouses();
-    storeFetchedColors();
+// GOT DATABASE INITIALIZER
+(async function () {
+    "use strict";
+    
+    // new indexDB database
+    var db = new Dexie("Lab8Database");
 
-    newColorsBtn.addEventListener("click", storeFetchedColors);
-    storeColorsBtn.addEventListener("click", storeFetchedColors);
-};
+    // database schema/structure
+    db.version(1).stores({
+        houses: `++id, &code, name, members`,
+        colors: `hex, rgb, hsl`
+    });
 
-// get GoT data
-async function getHouses() {
-    try {
-        let response = await fetch("houses.json");
-        let data = await response.json();
-        return data;
-    } catch(err) {
-        console.log("Retrieve Houses Err", err);
-    };
-};
+    const gotData = await fetch("../houses.json");
+    const gotResults = await gotData.json();
 
-// apply GoT data to HTML
-async function inputHouses() {
-    try {
-        let housesData = await getHouses();
+    db.houses.bulkPut(gotResults);
+    console.log("bulkPut", db.houses);
+    clearColsBtn.addEventListener("click", clearStorage(db));
 
-        //create a temp holder to append all the html generated inside the forEach iterator
+    const gotDatabase = await db.houses.orderBy("id").toArray();
+
+    const colorData = await fetch("https://x-colors.herokuapp.com/api/random?number=2");
+    const colorResults = await colorData.json();
+    const colorArray = [colorResults[0], colorResults[1]];
+
+    console.log("colors", colorArray);
+
+    db.colors.bulkPut(colorArray);
+
+    const colorDatabase = await db.colors.toArray();
+
+    const dbArray = [db.houses, db.colors];
+
+    await db
+        .transaction("rw", dbArray, async (tx) => {
+            let info = [];
+
+            info = [colorDatabase, gotDatabase];
+            return info;
+    })
+    .then((results) => {
+        console.log("results", results);
+        //temp holder for all the html generated inside the forEach iterator
         let allHouses = new DocumentFragment;
 
-        // iterate through house data to create a house list
-        housesData.forEach(house => {
-            // create elements
-            let houseList = document.createElement("dl");
-            let listTitle = document.createElement("dt");
-            // edit title content
-            listTitle.classList.add("house");
-            listTitle.innerHTML = house.name;
-            
-            // connect title to list
-            houseList.appendChild(listTitle);
+            // iterate through house data to create a house list
+            results[1].forEach((house) => {
+                // create elements
+                let houseList = document.createElement("dl");
+                let listTitle = document.createElement("dt");
+                // edit title content
+                listTitle.classList.add("house");
+                listTitle.innerHTML = house.name;
+                
+                // connect title to list
+                houseList.appendChild(listTitle);
 
-            // identify family members
-            let family = house.members;
+                // identify family members
+                let family = house.members;
 
-            //loop through family members
-            for (let member of family) {
-                // create list item (li)
-                let memberLI = document.createElement("dd");
-                // edit li content
-                memberLI.classList.add("member");
-                memberLI.innerHTML = member;
-                // add li to list
-                houseList.appendChild(memberLI);
-            };
+                //loop through family members
+                for (let member of family) {
+                    // create list item (li)
+                    let memberLI = document.createElement("dd");
+                    // edit li content
+                    memberLI.classList.add("member");
+                    memberLI.innerHTML = member;
+                    // add li to list
+                    houseList.appendChild(memberLI);
+                };
 
-            // add house to list
-            allHouses.appendChild(houseList);
-        });
+                // add house to list
+                allHouses.appendChild(houseList);
+            });
 
-        // reset section content
-        houses.innerHTML = "";
-        // add houses lists to section
-        houses.appendChild(allHouses);
+            // reset section content
+            houses.innerHTML = "";
+            // add houses lists to section
+            houses.appendChild(allHouses);
+            console.log(boxes[0].style.backgroundColor);
 
-    } catch(err) {
-        console.log("Input Error", err);
-    };
-};
+        // colors function
+        console.log("then colors", results[0]);
 
-async function getColors() {
+        for (let i = 0, colors = results[0]; i < boxes.length; i++) {
+            let color = colors[i].hex;
+            let box = boxes[i];
+            box.style.backgroundColor = color;
+        };
+
+        console.log();
+        console.log(boxes[0].style.backgroundColor);
+        
+    })
+    .catch((err) => {
+        console.log("IIFE err: " + err);
+    })
+} ());
+
+// GET COLORS *IN INIT F(X), NOT FROM DATABASE
+/*async function getColors() {
     try {
         let response = await fetch("https://x-colors.herokuapp.com/api/random?number=2");
         let data = await response.json();
@@ -87,110 +123,30 @@ async function getColors() {
             let box = boxes[i];
             box.style.backgroundColor = color;
         };
-        const d = new Date();
-        let time = [d.getTime()];
-        let results = data.concat(time);
-        return results;
+        console.log("function init", data);
+        return data;
     } catch(err) {
         console.log("Color Error", err);
     };
-};
+};*/
 
-async function storeFetchedColors() {
+
+// STORE COLORS IN LocalStorage
+/* async function storeFetchedColors() {
     try {
         let colors = await getColors();
         // check property and set value accordingly
         if (typeof Storage !== "undefined") {
-            var fetchColors = window.localStorage.fetchColors;
             var currentColors = window.localStorage.currentColors;
-            var previousColors = window.localStorage.previousColors;
-            if (typeof fetchColors == "undefined") {
-                // store these colors in local storage
-                window.localStorage.setItem("fetchColors", [0, colors[0].hex, colors[1].hex]);
-                window.localStorage.setItem("currentColors", [0, colors[0].hex, colors[1].hex]);
-            } else {
-                fetchColors = [0, colors[0].hex, colors[1].hex];
-                currentColors = [0, colors[0].hex, colors[1].hex];
-            };
-            console.log("fetch", fetchColors, "current", currentColors);
+            window.localStorage.setItem("currentColors", [0, colors[0].hex, colors[1].hex]);
+            console.log("current", currentColors);
         } else {
             document.getElementById("result").innerHTML = "Unfortunately your browser does not support web storage and so this button does not work";
         };
-        console.log("CURRENT: ", currentColors, "PREV: ", previousColors);
     } catch(err) {
         console.log("Storage Error", err);
     };
-};
-
-// GOT DATABASE INITIALIZER
-(async function () {
-    "use strict";
-    
-    // new indexDB database
-    var gotdb = new Dexie("GoTDatabase");
-
-    // database schema/structure
-    gotdb.version(1).stores({
-        houses: `++id, code, name, members`
-    });
-
-    await gotdb.houses.bulkPut([
-        { id: 1, code: "ST", name: "Stark", members: ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"] },
-        { id: 2, code: "LA", name: "Lannister", members: ["Tywin", "Cersei", "Jaime", "Tyrion"]},
-        { id: 3, code: "BA", name: "Baratheon", members: ["Robert", "Stannis", "Renly"] },
-        { id: 4, code: "TA", name: "Targaryen", members: ["Aerys", "Daenerys", "Viserys"] }
-    ]);
-
-    await gotdb
-        .transaction("readwrite", gotdb.houses, async (tx) => {
-            let info = [];
-
-            info = await gotdb.houses.orderBy("id").toArray();
-            return info;
-    })
-    .then((results) => {
-        //temp holder for all the html generated inside the forEach iterator
-        let allHouses = new DocumentFragment;
-
-        // iterate through house data to create a house list
-        results.forEach((house) => {
-            // create elements
-            let houseList = document.createElement("dl");
-            let listTitle = document.createElement("dt");
-            // edit title content
-            listTitle.classList.add("house");
-            listTitle.innerHTML = house.name;
-            
-            // connect title to list
-            houseList.appendChild(listTitle);
-
-            // identify family members
-            let family = house.members;
-
-            //loop through family members
-            for (let member of family) {
-                // create list item (li)
-                let memberLI = document.createElement("dd");
-                // edit li content
-                memberLI.classList.add("member");
-                memberLI.innerHTML = member;
-                // add li to list
-                houseList.appendChild(memberLI);
-            };
-
-            // add house to list
-            allHouses.appendChild(houseList);
-        });
-
-        // reset section content
-        houses.innerHTML = "";
-        // add houses lists to section
-        houses.appendChild(allHouses);
-    })
-    .catch((err) => {
-        console.log("IIFE err: " + err);
-    })
-} ());
+}; */
 
 /*
 fetch("colors.json")
